b9e9413ef6c0d1f7a16e472085cfe63402432229
Joseph <joeleger59@gmail.com>
2020-04-17 18:14:52 -0400

    learning how to create distribution packages and modules

--- mymodules/vsearch.py
+++ mymodules/vsearch.py
@@ -1,13 +1,10 @@
-# Annotation settings for function call
-# annotations are done for param, and function return value
-
 
 def search4vowels(phrase: str) -> set:
-    """ Return the found vowels in the word phrase """
+    """Return the found vowels in the word phrase"""
     vowels = set('aeiou')
     return vowels.intersection(set(phrase))
 
 
 def search4letters(letters: str, phrase: str = 'aeiou') -> set:
-    """Return a sorted phrase from a set of letters """
-    return sorted(set(letters).intersection(set(phrase)))
+    """Return a sorted phrase from a set of letters"""
+    return set(letters).intersection(set(phrase))
