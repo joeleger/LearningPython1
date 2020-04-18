6e29841b118fdee2f357b0d86a44eafb3baefc46
Joseph <joeleger59@gmail.com>
2020-04-18 19:33:45 -0400

    learning how to create distribution packages and modules

--- mymodules/setup.py
+++ mymodules/setup.py
@@ -1,9 +1,12 @@
 from setuptools import setup
 
-setup(name='vsearch',
-      version='1.0',
-      description='The Head First Python Search Tools',
-      author='HF Python 2e',
-      author_email='joeleger59@gmail.com',
-      url='headfirstlabs.com',
-      )
+setup(
+    name='vsearch',
+    version='1.0',
+    description='The Head First Python Search Tools',
+    author='HF Python 2e',
+    author_email='hfpy2e@gmail.com',
+    url='headfirstlabs.com',
+    py_modules=['vsearch'],
+)
+
