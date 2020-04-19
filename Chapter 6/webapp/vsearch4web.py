a68e1f6ea636ae5f9574ee2595a46df2234a9304
Joseph <joeleger59@gmail.com>
2020-04-19 16:01:13 -0400

    learning how to use Flask framework and saving to files IO and adding html output of the log file

--- Chapter 6/webapp/vsearch4web.py
+++ Chapter 6/webapp/vsearch4web.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 def log_request(req: 'flask_request', res: str) -> None:
     with open('vsearch.log.', 'a') as log:
-        print(str(dir(req)), res, file=log)
+        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
 
 
 @app.route('/search4', methods=['POST'])
@@ -32,10 +32,19 @@ def entry_page() -> 'html':
 
 
 @app.route('/viewlog')
-def view_the_log() -> 'str':
+def view_the_log() -> 'html':
+    contents = []
     with open('vsearch.log') as log:
-        contents = log.read()
-        return escape(contents)
+        for line in log:
+            contents.append([])
+            for item in line.split('|'):
+                contents[-1].append(escape(item))
+    titles = ('Form Data', 'Remote address', 'User agent', 'Results')
+    return render_template('viewlog.html',
+                           the_title='View Log',
+                           the_row_titles=titles,
+                           the_data=contents,
+                           )
 
 
 if __name__ == '__main__':
