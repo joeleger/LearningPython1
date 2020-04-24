7655a43adfb86fed0b836d8f6d7e6f8971bc6a9b
Joseph <joeleger59@gmail.com>
2020-04-24 12:24:31 -0400

    various sql queries

---
+++ Chapter 10/queries.sql
@@ -0,0 +1,13 @@
+select count(letters) as 'count', letters
+from log
+group by letters
+order by count desc
+limit 1;
+
+select distinct ip from log;
+
+select browser_string, count(browser_string) as 'count'
+from log
+group by browser_string
+order by count desc
+limit 1;
